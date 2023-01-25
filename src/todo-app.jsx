a7cb163ef6ec2d51700f3dcd5c86726d134b2a7e
Rahul N <rahul.eee32@gmail.com>
2023-01-25 16:55:39 +0530

    Code cleanup
    Removing all the demo files, adding a Todo component and updating the Readme file

---
+++ src/todo-app.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class TodoApp extends Component {
+  render() {
+    return (
+      <div>
+        Todo App
+      </div>
+    )
+  }
+}
+
+export default TodoApp;
