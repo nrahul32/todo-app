cb2129c8f9f0ff301e325f533dea2360ab01cd28
Rahul N <rahul.eee32@gmail.com>
2023-01-31 18:30:12 +0530

    Storing input entered in state
    Added input field and button in AddTodo component
    Initialized the state to store input entered
    Added handleChange function to set the state when input changes
    Added handleSubmit that alerts the latest state

--- src/add-todo.jsx
+++ src/add-todo.jsx
@@ -1,10 +1,27 @@
 import React, { Component } from "react";
 
 class AddTodo extends Component {
+
+    state = {
+        todo: 'Add text here'
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            todo: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        alert("State: " + this.state.todo);
+    }
+
     render() {
         return (
             <div>
-                Add Todo
+                Add Todo:
+                <input type='text' value={this.state.todo} onChange={this.handleChange}></input>
+                <button onClick={this.handleSubmit}>Add</button>
             </div>
         )
     }
